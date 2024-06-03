10deea6649b1954ce8d8823ce0beec5986424aa1
Rokas ツ <rokasgest@gmail.com>
2024-06-03 13:47:32 +0300

    added DTOs

---
+++ RestaurantOrdersManager.Core/DTO/OrderDTO/OrderAddRequest.cs
@@ -0,0 +1,25 @@
+﻿using RestaurantOrdersManager.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.DTO.OrderDTO
+{
+    public class OrderAddRequest
+    {
+        [Required(ErrorMessage ="provide creadetby")]
+        public int CreatedBy { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public DateTime TimeFinished { get; set; }
+        public ICollection<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
+        public required Enum OrderStatus { get; set; }
+
+        public Order ToOrder()
+        {
+            return new Order { CreatedBy = CreatedBy, TimeCreated = TimeCreated, OrderStatus = OrderStatus };
+        }
+    }
+}
---
+++ RestaurantOrdersManager.Core/DTO/OrderDTO/OrderResponse.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantOrdersManager.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.DTO.OrderDTO
+{
+    public class OrderResponse
+    {
+        public int OrderId { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime TimeCreated { get; set; }
+        public DateTime TimeFinished { get; set; }
+        public ICollection<MenuItem> MenuItems { get; set; } 
+        public Enum? OrderStatus { get; set; }
+    }
+    public static class OrderResponseExtensio
+    {
+        public static OrderResponse OrderResponse(this OrderResponse response)
+        {
+            return new OrderResponse { OrderId = response.OrderId, CreatedBy = response.CreatedBy, TimeCreated = response.TimeCreated, TimeFinished = response.TimeFinished,MenuItems = response.MenuItems, OrderStatus = response.OrderStatus };
+
+        }
+    }
+}
