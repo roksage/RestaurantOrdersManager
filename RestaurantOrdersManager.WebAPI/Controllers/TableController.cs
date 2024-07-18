02b66f54e7b3ee539b81eae792b60c8022556a27
Rokas ツ <rokasgest@gmail.com>
2024-07-18 08:42:37 +0300

    TableController class

---
+++ RestaurantOrdersManager.WebAPI/Controllers/TableController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantOrdersManager.WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
