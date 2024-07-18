a745ff74c5c94f7eba2c7b05411391497403d419
Rokas ツ <rokasgest@gmail.com>
2024-07-18 15:41:33 +0300

    Ingredients DTO

---
+++ RestaurantOrdersManager.Core/ServiceContracts/DTO/IngredientDTO/IngredientDeleteRequest.cs
@@ -0,0 +1,19 @@
+﻿using RestaurantOrdersManager.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.ServiceContracts.DTO.IngredientDTO
+{
+    public class IngredientDeleteRequest
+    {
+        public int IngredientId { get; set; } 
+
+        public Ingredient ToIngredient()
+        {
+            return new Ingredient { }
+        }
+    }
+}
---
+++ RestaurantOrdersManager.Core/ServiceContracts/DTO/IngredientDTO/IngredientUpdateRequest.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantOrdersManager.Core.Entities;
+using RestaurantOrdersManager.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.ServiceContracts.DTO.IngredientDTO
+{
+    public class IngredientUpdateRequest
+    {
+        public string IngredientName { get; set; }
+        public IngredientEnums IngredientUnit { get; set; }
+        public double IngredientAmount { get; set; }
+
+        public Ingredient ToIngredient()
+        {
+            return new Ingredient
+            {
+                IngredientName = IngredientName,
+                IngredientUnit = IngredientUnit,
+                IngredientAmount = IngredientAmount
+            };
+        }
+    }
+}
