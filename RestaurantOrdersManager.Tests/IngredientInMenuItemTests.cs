881cb90c968e82ae4c7409b295e55f64bcf0cc17
Rokas Gestautas <rokasgest@gmail.com>
2024-07-30 20:33:12 +0300

    test

---
+++ RestaurantOrdersManager.Tests/IngredientInMenuItemTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Tests
+{
+    public class Class1
+    {
+        private async Task<RestaurantOrdersDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<RestaurantOrdersDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new RestaurantOrdersDbContext(options);
+            databaseContext.Database.EnsureCreatedAsync();
+            databaseContext.Database.EnsureDeletedAsync();
+
+            var employee = new IngredientInMenuItem
+            {
+
+            };
+
+            databaseContext.Add(employee);
+            databaseContext.SaveChangesAsync();
+
+            return databaseContext;
+        }
+    }
+}
