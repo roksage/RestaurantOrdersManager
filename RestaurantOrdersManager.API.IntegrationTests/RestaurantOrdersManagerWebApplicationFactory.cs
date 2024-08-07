abf84f41f68a3e2ca6d5710ca503e06d8af5eeff
Rokas ツ <rokasgest@gmail.com>
2024-08-07 08:51:36 +0300

    a

--- RestaurantOrdersManager.API.IntegrationTests/RestaurantOrdersManagerWebApplicationFactory.cs
+++ RestaurantOrdersManager.API.IntegrationTests/RestaurantOrdersManagerWebApplicationFactory.cs
@@ -38,29 +38,3 @@ namespace RestaurantOrdersManager.API.IntegrationTests
         }
     }
 }
-
-        //protected override void ConfigureWebHost(IWebHostBuilder builder)
-        //{
-        //    builder.ConfigureTestServices(services =>
-        //    {
-        //        services.RemoveAll(typeof(DbContextOptions<RestaurantOrdersDbContext>));
-
-
-        //        var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-
-        //        services.AddSqlServer<RestaurantOrdersDbContext>(connectionString);
-
-        //        var dbContext = CreateDbContext(services);
-        //        dbContext.Database.EnsureCreated();
-        //        dbContext.Database.EnsureDeleted();
-
-        //    });
-        //}
-
-        //private static RestaurantOrdersDbContext CreateDbContext(IServiceCollection services)
-        //{
-        //    var serviceProvider = services.BuildServiceProvider();
-        //    var scope = serviceProvider.CreateScope();
-        //    var dbContext = scope.ServiceProvider.GetRequiredService<RestaurantOrdersDbContext>();
-        //    return dbContext;
-        //}
