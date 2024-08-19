302d728a0ec4f5c4e15d9f69d715cd91103ae8f5
Rokas ツ <rokas.gestautas@seb.se>
2024-08-19 11:43:23 +0300

    cooking station DTOs

---
+++ RestaurantOrdersManager.Core/ServiceContracts/RestaurantOrdersDTO/CookingStationDTO/CookingStationAddRequest.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantOrdersManager.Core.Entities.RestaurantOrders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.ServiceContracts.RestaurantOrdersDTO.CookingStationDTO
+{
+    public class CookingStationAddRequest
+    {
+        public string cookingStationName { get; set; }
+    }
+
+}
---
+++ RestaurantOrdersManager.Core/ServiceContracts/RestaurantOrdersDTO/CookingStationDTO/CookingStationResponse.cs
@@ -0,0 +1,31 @@
+﻿using RestaurantOrdersManager.Core.Entities.RestaurantOrders;
+using RestaurantOrdersManager.Core.ServiceContracts.DTO.MenuItemDTO;
+using RestaurantOrdersManager.Core.ServiceContracts.DTO.OrderDTO;
+using RestaurantOrdersManager.Core.ServiceContracts.DTO.OrderedMenuItemDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantOrdersManager.Core.ServiceContracts.RestaurantOrdersDTO.CookingStationDTO
+{
+    public class CookingStationResponse
+    {
+        public int cookingStationId { get; set; }
+        public string cookingStationName { get; set; }
+        public ICollection<MenuItemToOrderResponse> cookingStationOrders { get; set; }
+    }
+    public static class CookingStationResponseExtention
+    {
+        public static CookingStationResponse ToCookingStationResponse(this CookingStation request)
+        {
+            return new CookingStationResponse
+            {
+                cookingStationId = request.cookingStationId,
+                cookingStationName = request.cookingStationName,
+                cookingStationOrders = request.cookingStationOrders.Select(i => i.ToOrderedMenuItemResponse()).ToList()
+            };
+        }
+    }
+}
